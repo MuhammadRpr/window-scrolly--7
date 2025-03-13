2f07392dad65fe630d12d0d69dba71626364cdbf
MuhammadRpr <muhammadqu017@gmail.com>
2025-03-13 20:18:39 +0700

    selesai #7

---
+++ script.js
@@ -0,0 +1,17 @@
+// const nav = document.querySelector('.navbar')
+// window.addEventListener('scroll', fixNav)
+
+// function fixNav() {
+//     if (window.scrollY > 300) {
+//         nav.classList.add('active')
+//     } else {
+//         nav.classList.remove('active')
+//     }
+// }
+
+
+//1. posisi scroll pada sebuah window browser bisa dicari angka koordinatnya. Pada saat baru membuka sebuah halaman website, posisi scroll Y adalah 0
+
+//2. Warna dan ukuran padding nav berubah saat koordinat scroll Y sudah menyentuh atau lebih dari angka tertentu. Angka tertentu ini bisa dicoba-coba.
+
+//3. Event yang ke-trigger pada exercise ini adalah "scroll"
